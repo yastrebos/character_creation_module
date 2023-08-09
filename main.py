a581d66a902c4e92d2bd289d75541b812df1e2df
Сергей Ястребов <yastrebos@vk.com>
2023-08-09 09:30:01 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
     """Атака."""
-
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5+randint(3, 5)}')
@@ -20,7 +19,6 @@ def attack(char_name: str, char_class: str) -> str:
 
 def defence(char_name: str, char_class: str) -> str:
     """Функция защиты."""
-
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -45,10 +43,10 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-    """Тренировка
-    персонажа
     """
-
+    Тренировка.
+    персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -75,7 +73,6 @@ def start_training(char_name: str, char_class: str) -> str:
 
 def choice_char_class() -> str:
     """Выбирем персонажа."""
-
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
